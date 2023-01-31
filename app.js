2076bf5ff00d943bb061817fa32c060b9335cc3c
Diogo Guimaraes <mail@diogoguimaraes.pt>
2023-01-31 23:25:36 +0000

    clueless

--- app.js
+++ app.js
@@ -1,41 +1,44 @@
 const express = require('express');
+const expressLayouts = require("express-ejs-layouts"); 
 
 // We create our own server named app
 // Express server will be handling requests and responses
 const app = express();
 const port = 3000
-const path      = require("path");
-const expressLayouts = require("express-ejs-layouts"); 
+const path = require("path");
 
-app.use(express.static(path.join('public')))
 
-app.set("views", __dirname + "/views");
+
+
+app.set("views",path.join( __dirname + "/views"));
 app.set("view engine", "ejs");
 app.use(expressLayouts);
+
+app.use(express.static(path.join(__dirname, 'public')));
 
 
 // Index Route
 app.get('/', (request, response) => {
     console.log(request);
-    response.render(path.join('index'));
+    response.render('index');
   });
 
 // About Route
 app.get('/about', (request, response) => {
     console.log(request);
-    response.render(path.join('about'));
+    response.render('about');
   });
 
 // Works Route
 app.get('/works', (request, response) => {
     console.log(request);
-    response.sendFile(path.join('works'));
+    response.sendFile('works');
   });
 
   // Gallery Route
 app.get('/gallery', (request, response) => {
     console.log(request);
-    response.sendFile(path.join('gallery'));
+    response.sendFile('gallery');
   });
 
 
