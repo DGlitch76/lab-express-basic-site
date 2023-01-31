0ba8f2bb788f4d18b1017b95efff7635c738bfe3
Diogo Guimaraes <mail@diogoguimaraes.pt>
2023-01-31 23:12:30 +0000

    something went wrong

--- app.js
+++ app.js
@@ -4,36 +4,45 @@ const express = require('express');
 // Express server will be handling requests and responses
 const app = express();
 const port = 3000
+const path      = require("path");
+const expressLayouts = require("express-ejs-layouts"); 
 
-app.use(express.static('public'))
+app.use(express.static(path.join('public')))
 
-app.use(express.static('views'));
+app.set("views", __dirname + "/views");
+app.set("view engine", "ejs");
+app.use(expressLayouts);
 
 
 // Index Route
-app.get('/home', (request, response, next) => {
+app.get('/', (request, response) => {
     console.log(request);
-    response.sendFile(__dirname + '/views/index.html');
+    response.render(path.join('index'));
   });
 
 // About Route
-app.get('/about', (request, response, next) => {
+app.get('/about', (request, response) => {
     console.log(request);
-    response.sendFile(__dirname + '/views/about.html');
+    response.render(path.join('about'));
   });
 
 // Works Route
-app.get('/works', (request, response, next) => {
+app.get('/works', (request, response) => {
     console.log(request);
-    response.sendFile(__dirname + '/views/works.html');
+    response.sendFile(path.join('works'));
   });
 
   // Gallery Route
-app.get('/gallery', (request, response, next) => {
+app.get('/gallery', (request, response) => {
     console.log(request);
-    response.sendFile(__dirname + '/views/gallery.html');
+    response.sendFile(path.join('gallery'));
   });
 
 
 // Server Started
-app.listen(3000, () => console.log('My first app listening on port 3000! '));
+app.listen(port, () => console.log('My first app listening on port 3000! '));
+
+
+
+
+
